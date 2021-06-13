64d289e7a8c4242a6177f55bc156329e070c7c02
batuhan ozdol <43928815+batuhanozdol@users.noreply.github.com>
2021-06-13 17:48:34 +0300

    Update game.js

--- static/js/game.js
+++ static/js/game.js
@@ -66,7 +66,7 @@ let questions = [
         answer: 1,
     },
     {
-        question: "<audio controls><source src='static/appass_3_format0.ogg' /></audio>",
+        question: "<audio controls><source src='static/appass_1_format0.mp3' /></audio>",
         choice1: "Human",
         choice2: "AI",
         answer: 1,
